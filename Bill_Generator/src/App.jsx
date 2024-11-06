ebd92f2de0ab028292f78bf4abdebeabed649e88
JM1892 <jeetmistry00@gmail.com>
2024-11-06 07:57:58 +0530

    day-1

---
+++ Bill_Generator/src/App.jsx
@@ -0,0 +1,21 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <header>
+          <h1>Bill Generator</h1>
+        </header>
+        <main>
+          <p>Welcome to the Bill Generator Application</p>
+          {/** placeholder for form and bill summary */}
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
